1a2bdfe6f4ab40176835a0527b81abaa3df5e2ff
Xander Whitby <xwhitby20@gmail.com>
2025-05-21 11:17:07 -0600

    made socketfd for connection

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <sys/socket.h>
-#include <string.h>
+#include <string.h> 
 #include <stdlib.h>
 #include <ctype.h>
 
-void makeSocket(){
-  socket1 = socket(domain, type, protocol);
-  return;
-}
-
 int main(){
+
+  socketfd = socket(AF_UNIX, SOCK_STREAM, );
   return 0;
 }
+
+                   
