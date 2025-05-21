72e067b6469d2791981ac3fe45bb4613d491aa05
Xander Whitby <xwhitby20@gmail.com>
2025-05-21 10:12:43 -0600

    first tcp commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void connect(){
+  return ;
+}
+
+void processData(){
+  return ;
+}
+
+void sendData(){
+  return ;
+}
+
+void endConnection(){
+  return ;
+}
+
+int main(){
+  printf("Program compiles\n");
+  return 0;
+} 
