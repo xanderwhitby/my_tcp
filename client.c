8d408e25a39e1e09090d3845f841ab46b0b1ca85
Xander Whitby <xwhitby20@gmail.com>
2025-05-22 08:30:42 -0600

    added function to client to pass package to sserver

---
+++ client.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <string.h> 
+#include <stdlib.h>
+#include <ctype.h>
+
+
+int main(){
+  // this function should get package and send it to the server
+  return 0;
+}
+
