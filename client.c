ef9f8736654da725ea13dc187b7b169d8ebc7cfa
Xander Whitby <xwhitby20@gmail.com>
2025-05-22 11:33:18 -0600

    added connect function to client and made client socket

--- client.c
+++ client.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
-#include <sys/socket.h>
-#include <string.h> 
 #include <stdlib.h>
-#include <ctype.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#define PORT 8080
 
 
 int main(){
   // this function should get package and send it to the server
+  int protocol = 0;
+  int clientfd;
+
+  clientfd = socket(AF_UNIX, SOCK_STREAM, protocol);
+  int connect(clientfd, const struct sockaddr *addr, socket_len addr_len);
+
+
   return 0;
 }
 
