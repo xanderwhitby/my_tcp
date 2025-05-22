302ec1ec0795cfb4844ce9632b71bc27534dceb6
Xander Whitby <xwhitby20@gmail.com>
2025-05-22 11:28:01 -0600

    changed everything to the main function for easier imports

--- main.c
+++ main.c
@@ -4,29 +4,28 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
+#define PORT 8080
 
 
-int makeProtocol(){
-
+int main(){
+// get the client protocol 
   int protocol = 0;
   int socketfd;
+  int backlog, buffer[1024];
+  int flags;
   
   socketfd = socket(AF_UNIX, SOCK_STREAM, protocol);
   if (socketfd == -1){
     printf("%s", "Socket not made");
   }
 
-  int bind(int socketfd, const struct sockaddr *addr, socketlen_ addrlen);
+  int bind(int socketfd,  const struct sockaddr *addr, socketlen_t addrlen);
+
+  int listen(socketfd, backlog);
   
+  int accept(socketfd, struct sockaddr *_Nullable restrict addr, socketlen_t *_Nullable restrict addrlen);
 
-
-}
-
-
-int main(){
-// get the client protocol 
     return 0;
-
 
 }
 
